b3d421ffe02a81cea84dd057a231490c3edaa4af
Andrey Rudenko <andrey.rudenko@gmail.com>
2017-05-24 01:12:47 +0300

    Implement GitHub authorization.

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,46 @@
 <?php
-// Routes
+
+use League\OAuth2\Client\Provider\GithubResourceOwner;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+// Routes.
+
+// GitHub authorization.
+$app->get('/auth', function(ServerRequestInterface $request, ResponseInterface $response, $args) {
+  $provider = new \League\OAuth2\Client\Provider\Github([
+    'clientId' => '587b512a6831b5acae95',
+    'clientSecret' => '170da7bfe3a52a13d249c956fc0f9e921ea4d147',
+    'redirectUri' => 'http://tesonet-test.local/auth',
+  ]);
+
+  $query = $request->getQueryParams();
+
+  if (!isset($query['code'])) {
+    $authUrl = $provider->getAuthorizationUrl();
+    $_SESSION['last_state'] = $provider->getState();
+    return $response->withStatus(302)->withHeader('Location', $authUrl);
+  }
+  elseif ((!isset($query['state'])) || ($query['state'] !== $_SESSION['last_state'])) {
+    unset($_SESSION['last_state']);
+    throw new Exception('Invalid state!');
+  }
+  else {
+    // Try to get an access token (using the authorization code grant).
+    $token = $provider->getAccessToken('authorization_code', ['code' => $query['code']]);
+    $_SESSION['token'] = $token;
+
+    try {
+      /** @var GithubResourceOwner $user */
+      $user = $provider->getResourceOwner($token);
+      $_SESSION['user'] = $user;
+    } catch (Exception $e) {
+      throw new Exception('Failed to get user details!');
+    }
+
+    return $response->withStatus(302)->withHeader('Location', '/issues/open');
+  }
+});
 
 // Front page.
 $app->get('/', function($request, $response, $args) {
@@ -9,15 +50,33 @@ $app->get('/', function($request, $response, $args) {
 });
 
 // Opened issues.
-$app->get('/issues/open', function($request, $response, $args) {
+$app->get('/issues/open', function(ServerRequestInterface $request, ResponseInterface $response, $args) {
+  if (!isset($_SESSION['token'])) {
+    return $response->withStatus(302)->withHeader('Location', '/');
+  }
+
+  /** @var GithubResourceOwner $user */
+  $user = $_SESSION['user'];
+
   /** @var Twig_Environment $twig */
   $twig = $this->twig;
-  return $twig->render('issues/open.html.twig', $args);
+  return $twig->render('issues/open.html.twig', [
+    'user' => $user,
+  ]);
 });
 
 // Closed issues.
-$app->get('/issues/open', function($request, $response, $args) {
+$app->get('/issues/closed', function(ServerRequestInterface $request, ResponseInterface $response, $args) {
+  if (!isset($_SESSION['token'])) {
+    return $response->withStatus(302)->withHeader('Location', '/');
+  }
+
+  /** @var GithubResourceOwner $user */
+  $user = $_SESSION['user'];
+
   /** @var Twig_Environment $twig */
   $twig = $this->twig;
-  return $twig->render('issues/closed.html.twig', $args);
+  return $twig->render('issues/closed.html.twig', [
+    'user' => $user,
+  ]);
 });
