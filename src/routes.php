74a8663d169925b10baab0f6276ccf4ff492f450
Andrey Rudenko <andrey.rudenko@gmail.com>
2017-05-24 00:05:12 +0300

    Create routes and templates for opened and closed issues lists.

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,23 @@
 <?php
 // Routes
-$app->get('/[{name}]', function ($request, $response, $args) {
+
+// Front page.
+$app->get('/', function($request, $response, $args) {
   /** @var Twig_Environment $twig */
   $twig = $this->renderer;
-  return $twig->render('base.html.twig', $args);
+  return $twig->render('index.html.twig', $args);
+});
+
+// Opened issues.
+$app->get('/issues/open', function($request, $response, $args) {
+  /** @var Twig_Environment $twig */
+  $twig = $this->renderer;
+  return $twig->render('issues/open.html.twig', $args);
+});
+
+// Closed issues.
+$app->get('/issues/open', function($request, $response, $args) {
+  /** @var Twig_Environment $twig */
+  $twig = $this->renderer;
+  return $twig->render('issues/closed.html.twig', $args);
 });
